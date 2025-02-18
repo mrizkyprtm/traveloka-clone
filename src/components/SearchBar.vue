088962de42638edf3a2311f66ab2597d057bcfad
mrizkyprtm <m.rizkypratama319@gmail.com>
2025-02-19 00:02:01 +0700

    assign emits to variable

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref } from "vue";
 
-defineEmits(["onSearch"]);
+const emit = defineEmits(["onSearch"]);
 
 const filter = ref({
   name: "",
@@ -12,39 +12,41 @@ const onSearch = () => {
 };
 </script>
 <template>
-  <div class="flex flex-auto wrapper-form">
-    <div
-      class="w-1/3 px-2 py-3 border rounded-l-md border-gray-300 transition-colors hover:border-gray-900 flex items-center gap-x-2">
-      <i class="pi pi-map-marker text-lg text-blue-500 ml-2"></i>
-      <input
-        type="text"
-        v-model="filter.name"
-        placeholder="Kota, hotel atau tempat wisata"
-        class="w-full focus:outline-none outline-0" />
+  <form @submit.prevent>
+    <div class="flex flex-auto wrapper-form">
+      <div
+        class="w-1/3 px-2 py-3 border rounded-l-md border-gray-300 transition-colors hover:border-gray-900 flex items-center gap-x-2">
+        <i class="pi pi-map-marker text-lg text-blue-500 ml-2"></i>
+        <input
+          type="text"
+          v-model="filter.name"
+          placeholder="Kota, hotel atau tempat wisata"
+          class="w-full focus:outline-none outline-0" />
+      </div>
+      <div
+        class="w-1/3 px-2 py-3 border border-gray-300 transition-colors hover:border-gray-900 flex items-center gap-x-2">
+        <i class="pi pi-calendar text-lg text-blue-500 ml-2"></i>
+        <input
+          type="text"
+          placeholder="24 Jan - 25 Jan, 1 malam"
+          class="w-full focus:outline-none" />
+      </div>
+      <div
+        class="w-1/3 px-2 py-3 border border-gray-300 transition-colors hover:border-gray-900 flex items-center gap-x-2">
+        <i class="pi pi-users text-lg text-blue-500 ml-2"></i>
+        <input
+          type="text"
+          placeholder="1 Dewasa, 0 Anak, 1 Kamar"
+          class="w-full focus:outline-none" />
+      </div>
+      <div class="">
+        <button
+          class="bg-blue-500 text-white btn-search h-full px-3 rounded-r-md font-bold text-nowrap flex items-center gap-x-2"
+          @click="onSearch">
+          <i class="pi pi-search text-lg text-white"></i>
+          Cari Hotel
+        </button>
+      </div>
     </div>
-    <div
-      class="w-1/3 px-2 py-3 border border-gray-300 transition-colors hover:border-gray-900 flex items-center gap-x-2">
-      <i class="pi pi-calendar text-lg text-blue-500 ml-2"></i>
-      <input
-        type="text"
-        placeholder="24 Jan - 25 Jan, 1 malam"
-        class="w-full focus:outline-none" />
-    </div>
-    <div
-      class="w-1/3 px-2 py-3 border border-gray-300 transition-colors hover:border-gray-900 flex items-center gap-x-2">
-      <i class="pi pi-users text-lg text-blue-500 ml-2"></i>
-      <input
-        type="text"
-        placeholder="1 Dewasa, 0 Anak, 1 Kamar"
-        class="w-full focus:outline-none" />
-    </div>
-    <div class="">
-      <button
-        class="bg-blue-500 text-white btn-search h-full px-3 rounded-r-md font-bold text-nowrap flex items-center gap-x-2"
-        @click="onSearch">
-        <i class="pi pi-search text-lg text-white"></i>
-        Cari Hotel
-      </button>
-    </div>
-  </div>
+  </form>
 </template>
