34a5bf5204aa83e2eba5d03c77d120b2c5b72c20
mrizkyprtm <m.rizkypratama319@gmail.com>
2025-02-19 01:35:55 +0700

    feat(hotel-details): layouting and integrate dynamic data hotel

--- src/pages/HotelDetailsView.vue
+++ src/pages/HotelDetailsView.vue
@@ -1,4 +1,214 @@
-<script setup></script>
+<script setup>
+import SearchBar from "@/components/SearchBar.vue";
+import { onMounted, ref } from "vue";
+import { useHotel } from "@/stores/useHotel";
+import { useRoute, useRouter } from "vue-router";
+
+const hotel = ref({
+  name: "Bukit Vipassana Hotel",
+  location: "Lembang, Bandung",
+  rating: 8.4,
+  reviews: 1160,
+  price: 368569,
+  images: [
+    "/img/hotel.jpeg",
+    "/img/hotel.jpeg",
+    "/img/hotel.jpeg",
+    "/img/hotel.jpeg",
+    "/img/hotel.jpeg",
+    "/img/hotel.jpeg",
+    "/img/hotel.jpeg",
+  ],
+  description: "Hotel ini menawarkan kenyamanan dan fasilitas terbaik untuk Anda.",
+  address:
+    "JL. Kolonel Masturi No. 99, Lembang, Bandung, Jawa Barat, Indonesia, 40391",
+  amenities: ["Restoran", "Kolam Renang", "Resepsionis 24 Jam", "WiFi", "Parkir"],
+});
+
+const { getHotelById } = useHotel();
+const route = useRoute();
+
+const result = ref("");
+onMounted(() => {
+  result.value = getHotelById(route.params.id);
+  // console.log(result.value);
+  hotel.value.name = result.value.name;
+  hotel.value.location = result.value.location;
+});
+
+const room = {
+  name: "Superior Twin Bed",
+  imageUrl: "/img/hotel.jpeg",
+  size: "24.0 m²",
+  options: [
+    {
+      id: 1,
+      name: "Tanpa Sarapan",
+      description: "1 ranjang twin",
+      guests: 2,
+      price: 368569,
+      originalPrice: 491125,
+      breakfastIncluded: false,
+    },
+    {
+      id: 2,
+      name: "Termasuk sarapan untuk 1 orang",
+      description: "1 ranjang twin",
+      guests: 1,
+      price: 417709,
+      originalPrice: 556945,
+      breakfastIncluded: true,
+    },
+  ],
+};
+</script>
 <template>
-  <div>Hotel Detail</div>
+  <div
+    class="sticky top-0 flex-1 py-3 bg-white shadow z-10 flex justify-center items-center">
+    <div class="container mx-auto max-w-7xl px-6">
+      <SearchBar></SearchBar>
+    </div>
+  </div>
+
+  <div class="container mx-auto max-w-7xl px-6 py-6 bg-white">
+    <div class="flex gap-4 warpper-content">
+      <!-- Main Image -->
+      <div class="w-1/2 m-img">
+        <img
+          :src="hotel.images[0]"
+          :alt="hotel.name"
+          class="w-full h-full object-cover" />
+      </div>
+
+      <!-- Image Gallery -->
+      <div class="flex-auto grid grid-cols-2 md:grid-cols-3 gap-4">
+        <div
+          v-for="image in hotel.images.slice(1)"
+          :key="image"
+          class="overflow-hidden shadow-md flex-auto">
+          <img :src="image" :alt="hotel.name" class="w-full h-full object-cover" />
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="container mx-auto max-w-7xl px-4 py-6">
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+      <div class="card">
+        <h2 class="text-2xl font-bold mb-4">{{ hotel.name }}</h2>
+        <p class="text-gray-600 mb-4">{{ hotel.location }}</p>
+      </div>
+
+      <div class="card">
+        <!-- Price and Booking Button -->
+        <div class="flex gap-4 items-center justify-end">
+          <div class="flex flex-col items-end">
+            <label for="">Harga/kamar/malam mulai dari</label>
+            <p class="text-xl font-bold text-orange-500">Rp {{ hotel.price }}</p>
+          </div>
+          <button class="bg-orange-500 text-white rounded-md py-2 px-4">
+            Pilih Kamar
+          </button>
+        </div>
+      </div>
+    </div>
+
+    <!-- Alert Message -->
+    <div class="bg-blue-100 p-4 rounded-md mb-4">
+      <p class="text-blue-700">
+        Jangan lewatkan! Sisa 2 kamar untuk harga paling murah.
+      </p>
+    </div>
+
+    <!-- Grid Layout for Cards -->
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
+      <!-- Rating Card -->
+      <div class="bg-white rounded-lg shadow-md p-4">
+        <h3 class="text-lg font-semibold">Mengesankan</h3>
+        <div class="flex items-center">
+          <span class="text-blue-500 font-bold text-2xl">{{ hotel.rating }}</span>
+          <span class="text-gray-500 text-sm">
+            (Dari {{ hotel.reviews }} review tamu yang terverifikasi)</span
+          >
+        </div>
+      </div>
+
+      <!-- Accommodation Card -->
+      <div class="bg-white rounded-lg shadow-md p-4">
+        <h3 class="text-lg font-semibold">Area Akomodasi</h3>
+        <p class="text-gray-600">{{ hotel.address }}</p>
+        <button class="text-blue-500 underline">Lihat Peta</button>
+      </div>
+
+      <!-- Facilities Card -->
+      <div class="bg-white rounded-lg shadow-md p-4">
+        <h3 class="text-lg font-semibold">Fasilitas Utama</h3>
+        <ul class="list-disc pl-5">
+          <li v-for="amenity in hotel.amenities" class="text-gray-600">
+            {{ amenity }}
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <!-- Description Section -->
+    <div class="bg-gray-100 p-4 rounded-md">
+      <p class="text-gray-700">{{ hotel.description }}</p>
+    </div>
+  </div>
+
+  <!-- room -->
+  <div class="container mx-auto max-w-7xl px-4 py-4" id="room-details">
+    <div class="flex flex-col md:flex-row">
+      <!-- Left Side: Room Information -->
+      <div class="md:w-1/4 mb-6 md:mb-0">
+        <h2 class="text-2xl font-bold mb-2">{{ room.name }}</h2>
+        <div class="w-full">
+          <img
+            :src="room.imageUrl"
+            :alt="room.name"
+            class="w-full h-48 object-cover rounded-lg mb-4" />
+        </div>
+        <p class="text-gray-600">Ukuran: {{ room.size }}</p>
+      </div>
+
+      <!-- Right Side: Room Options Table -->
+      <div class="flex-auto flex justify-center items-center p-6">
+        <table class="min-w-full bg-white border border-gray-300">
+          <thead>
+            <tr class="bg-gray-100">
+              <th class="py-2 px-4 border-b text-left">Pilihan Kamar</th>
+              <th class="py-2 px-4 border-b text-center">Tamu</th>
+              <th class="py-2 px-4 border-b text-center">Harga/kamar/malam</th>
+              <th class="py-2 px-4 border-b"></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="option in room.options">
+              <td class="py-2 px-4 border-b">
+                {{ option.name }}<br />
+                <small class="text-gray-500">
+                  {{ option.description }}
+                </small>
+              </td>
+              <td class="py-2 px-4 border-b text-center">
+                {{ option.guests }} orang
+              </td>
+              <td class="py-2 px-4 border-b text-center">
+                <span class="line-through text-gray-500"
+                  >Rp {{ option.originalPrice }}</span
+                ><br />
+                <span class="text-orange-500 font-bold">Rp {{ option.price }}</span>
+              </td>
+              <td class="py-2 px-4 border-b">
+                <button class="bg-blue-500 text-white rounded-md py-1 px-2">
+                  Pilih
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
 </template>
