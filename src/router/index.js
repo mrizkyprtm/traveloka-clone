b970fa48c3d9f487c8d65dfd66565c8c951cc497
mrizkyprtm <m.rizkypratama319@gmail.com>
2025-02-19 16:29:02 +0700

    enable route hash navigation

--- src/router/index.js
+++ src/router/index.js
@@ -21,7 +21,14 @@ const router = createRouter({
     },
   ],
   scrollBehavior(to, from, savedPosition) {
+    if (to.hash) {
+      return {
+        el: to.hash,
+        behavior: "smooth",
+      };
+    } else {
     return savedPosition || { top: 0, behavior: "smooth" };
+    }
   },
 });
 
