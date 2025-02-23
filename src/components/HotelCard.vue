8815e7a92fd30db0a3501b72975ac95773a1ab8e
mrizkyprtm <m.rizkypratama319@gmail.com>
2025-02-24 01:19:50 +0700

    refactor(hotel-card): improve layout and styling for result hotel display

--- src/components/HotelCard.vue
+++ src/components/HotelCard.vue
@@ -16,9 +16,10 @@ const toDetails = (id) => {
 
 <template>
   <div class="hotel-card">
-    <div class="card flex bg-white rounded-lg shadow-md overflow-clip">
+    <div
+      class="card flex bg-white rounded-lg shadow-md hover:shadow-sky-200 transition overflow-clip">
       <!-- Gambar -->
-      <div class="w-1/3 flex flex-col gap-2">
+      <div class="w-1/3 flex flex-col gap-1">
         <div class="top-img w-full">
           <img
             src="/img/hotel.jpeg"
@@ -26,7 +27,7 @@ const toDetails = (id) => {
             class="w-full h-auto" />
         </div>
 
-        <div class="bottom-img flex gap-2">
+        <div class="bottom-img flex gap-1">
           <div class="wrapper-img flex-auto">
             <img
               src="/img/hotel.jpeg"
@@ -50,26 +51,34 @@ const toDetails = (id) => {
 
       <!-- Informasi Hotel -->
       <div class="flex-1 flex">
-        <div class="flex-1 p-4">
-          <h2 class="text-lg font-bold text-gray-800">{{ hotel.name }}</h2>
-          <p class="text-sm text-gray-600">{{ hotel.location }}</p>
+        <div class="flex-1 flex flex-col p-4 gap-1">
+          <div class="flex justify-between">
+            <h3 class="text-lg font-bold text-gray-800">
+              {{ hotel.name }}
+            </h3>
+            <div class="text-sm text-gray-500 font-semibold">
+              <span class="font-bold text-blue-500">
+                {{ parseFloat(hotel.rating).toFixed(1) }}
+              </span>
+              ({{ hotel.reviews }} ulasan)
+              <p class="text-right text-xs">Mengesankan</p>
+            </div>
+          </div>
+          <p class="text-sm text-gray-600 flex items-center gap-1">
+            <i class="pi pi-map-marker text-xs"></i>
+            {{ hotel.location }}
+          </p>
           <div class="mt-2 flex flex-wrap gap-2 text-sm text-blue-600">
             <Chip
               v-for="amneti in hotel.amenities"
               :label="amneti"
-              class="bg-blue-100 py-1 text-sm" />
-          </div>
-          <div class="mt-2 text-sm text-gray-500">
-            <span class="font-semibold text-blue-500">
-              {{ parseFloat(hotel.rating).toFixed(1) }}
-            </span>
-            ({{ hotel.reviews }} ulasan) Mengesankan
+              class="bg-blue-100 py-1 text-xs" />
           </div>
         </div>
 
         <!-- Harga dan Tombol -->
         <div
-          class="text-right flex flex-col shadow-left p-4 items-end justify-end">
+          class="text-right flex flex-col shadow-md p-4 items-end justify-end">
           <div class="text-green-600 text-sm font-semibold">
             {{ hotel.discount }}% OFF
           </div>
@@ -83,11 +92,15 @@ const toDetails = (id) => {
             Sisa 2 kamar di harga ini!
           </p>
           <Button
+            label="Pilih Kamar"
             class="mt-4 ml-auto font-bold"
             severity="warn"
-            @click="toDetails(hotel.id)">
-            Pilih Kamar
-          </Button>
+            @click="toDetails(hotel.id)"
+            :pt="{
+              label: {
+                class: 'font-bold',
+              },
+            }" />
         </div>
       </div>
     </div>
